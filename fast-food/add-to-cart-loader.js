0c03deea6cf9f45b9b8294e5451f09d5b78e55a4
peca02 <djordjevicpredrag2002@gmail.com>
2025-02-27 02:44:40 +0100

    Create add-to-cart-loader.js

---
+++ fast-food/add-to-cart-loader.js
@@ -0,0 +1,3 @@
+const script = document.createElement("script");
+script.src = `https://cdn.jsdelivr.net/gh/peca02/js-scripts@latest/fast-food/add-to-cart.js?v=${Date.now()}`;
+document.body.appendChild(script);
