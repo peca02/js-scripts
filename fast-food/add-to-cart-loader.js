f7b6bcb95e50efadc106e28faea28d7c88e951fc
peca02 <djordjevicpredrag2002@gmail.com>
2025-02-27 03:08:02 +0100

    Update add-to-cart-loader.js

--- fast-food/add-to-cart-loader.js
+++ fast-food/add-to-cart-loader.js
@@ -1,3 +1,6 @@
+window.onerror = function(message, source, lineno, colno, error) {
+    console.error(`⚠️ Greška u skripti: ${message} (Linija: ${lineno}, Kolona: ${colno})`);
+};
 const script = document.createElement("script");
 script.src = `https://cdn.jsdelivr.net/gh/peca02/js-scripts@latest/fast-food/add-to-cart.js?v=${Date.now()}`;
 document.body.appendChild(script);
