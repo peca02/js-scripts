27a48a25c5cfb0fa5a3fd5bd48e22257a4e78c54
peca02 <djordjevicpredrag2002@gmail.com>
2025-02-27 03:07:44 +0100

    Update add-to-cart.js

--- fast-food/add-to-cart.js
+++ fast-food/add-to-cart.js
@@ -1,7 +1,1 @@
-window.onerror = function(message, source, lineno, colno, error) {
-    console.error(`⚠️ Greška u skripti: ${message} (Linija: ${lineno}, Kolona: ${colno})`);
-};
-
-// Ostatak koda tvoje skripte
-
 console.log("Skripta se uspešno učitala!");
