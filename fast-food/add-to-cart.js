8d0252b43e876b234bcd74e933ce1101cc11939f
peca02 <djordjevicpredrag2002@gmail.com>
2025-03-02 19:12:43 +0100

    Update add-to-cart.js

--- fast-food/add-to-cart.js
+++ fast-food/add-to-cart.js
@@ -59,6 +59,10 @@ Webflow.push(function () {
             gridContainer.innerHTML = ''; // Clear existing content
 
             // Add header items with separate divs for each element
+            const blankDiv = document.createElement('div');
+            blankDiv.classList.add('ff-cart-dislay-grid-header');
+            blankDiv.innerText = '';
+            
             const imageHeaderDiv = document.createElement('div');
             imageHeaderDiv.classList.add('ff-cart-dislay-grid-header');
             imageHeaderDiv.innerText = 'Image';
@@ -76,6 +80,7 @@ Webflow.push(function () {
             amountHeaderDiv.innerText = 'Amount';
 
             // Append header items to the grid container
+            gridContainer.appendChild(blankDiv);
             gridContainer.appendChild(imageHeaderDiv);
             gridContainer.appendChild(productHeaderDiv);
             gridContainer.appendChild(priceHeaderDiv);
@@ -83,12 +88,28 @@ Webflow.push(function () {
 
             // Render each cart item directly into the grid container
             cartItems.forEach(item => {
+                const svgNS = "http://www.w3.org/2000/svg";
+                const svgElement = document.createElementNS(svgNS, "svg");
+                const gElement = document.createElementNS(svgNS, "g");
+                const path1 = document.createElementNS(svgNS, "path");
+                const path2 = document.createElementNS(svgNS, "path");
                 const image = document.createElement('img');
                 const nameDiv = document.createElement('div');
                 const priceDiv = document.createElement('div');
                 const amountDiv = document.createElement('div');
 
                 // Set content for each cell
+                svgElement.setAttribute("xmlns", svgNS);
+                svgElement.setAttribute("fill", "currentColor");
+                svgElement.setAttribute("width", "100%");
+                svgElement.setAttribute("viewBox", "0 0 408.483 408.483");
+                svgElement.setAttribute("class", "ff-cart-display-recycle-bin");
+                path1.setAttribute("d", "M87.748,388.784c0.461,11.01,9.521,19.699,20.539,19.699h191.911c11.018,0,20.078-8.689,20.539-19.699l13.705-289.316 H74.043L87.748,388.784z M247.655,171.329c0-4.61,3.738-8.349,8.35-8.349h13.355c4.609,0,8.35,3.738,8.35,8.349v165.293 c0,4.611-3.738,8.349-8.35,8.349h-13.355c-4.61,0-8.35-3.736-8.35-8.349V171.329z M189.216,171.329 c0-4.61,3.738-8.349,8.349-8.349h13.355c4.609,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.737,8.349-8.349,8.349h-13.355 c-4.61,0-8.349-3.736-8.349-8.349V171.329L189.216,171.329z M130.775,171.329c0-4.61,3.738-8.349,8.349-8.349h13.356 c4.61,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.738,8.349-8.349,8.349h-13.356c-4.61,0-8.349-3.736-8.349-8.349V171.329z");
+                path2.setAttribute("d", "M343.567,21.043h-88.535V4.305c0-2.377-1.927-4.305-4.305-4.305h-92.971c-2.377,0-4.304,1.928-4.304,4.305v16.737H64.916 c-7.125,0-12.9,5.776-12.9,12.901V74.47h304.451V33.944C356.467,26.819,350.692,21.043,343.567,21.043z");
+                gElement.appendChild(path1);
+                gElement.appendChild(path2);
+                svgElement.appendChild(gElement);
+                
                 image.src = item.productImageUrl;
                 image.alt = item.productName;
                 image.classList.add('ff-cart-display-item-image');
@@ -97,6 +118,7 @@ Webflow.push(function () {
                 amountDiv.innerText = item.amount;
 
                 // Append each cell directly to the grid container
+                gridContainer.appendChild(svgElement);
                 gridContainer.appendChild(image);
                 gridContainer.appendChild(nameDiv);
                 gridContainer.appendChild(priceDiv);
