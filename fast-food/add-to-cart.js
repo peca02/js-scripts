9107fb949d90ccd13a97ee659cd9f8659e0a951e
peca02 <djordjevicpredrag2002@gmail.com>
2025-03-03 04:07:24 +0100

    Update add-to-cart.js

--- fast-food/add-to-cart.js
+++ fast-food/add-to-cart.js
@@ -200,6 +200,7 @@ function renderCartItems() {
                 updateCartInLocalStorage(cartItems);
                 updateCartNumber();
                 updateTotalPrice();
+                renderCartItems();
                 }
             });
 
