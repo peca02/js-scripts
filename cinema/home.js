81e5fe2ad43f151005fea60c39ff6dc83e20bc29
peca02 <djordjevicpredrag2002@gmail.com>
2025-04-20 00:06:28 +0200

    Update home.js

--- cinema/home.js
+++ cinema/home.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+const { createClient } = await import('https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm')
 
 const supabaseUrl = 'https://ypestrqwjqmgkpidrdct.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlwZXN0cnF3anFtZ2twaWRyZGN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1ODQ3MzIsImV4cCI6MjA2MDE2MDczMn0.jIpa-xwIDEdcqBIAFW4NBAtPIvdQRLiaKUwp51p_HkY'
