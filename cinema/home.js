d46c98cf78b026ded3291f40d5946cae296051ff
peca02 <djordjevicpredrag2002@gmail.com>
2025-04-21 01:29:40 +0200

    Update home.js

--- cinema/home.js
+++ cinema/home.js
@@ -131,7 +131,7 @@ cinemaElements.forEach(el => {
     renderMovies(filteredMovies);
 
     // 4. Triggeruj Webflow dropdown zatvaranje
-    document.querySelector('.c-dropdown-toggle').click(); // Ovo simulira klik van dropdowna i Webflow ga sam zatvara
+    document.querySelector('#w-dropdown-toggle-0').click(); // Ovo simulira klik van dropdowna i Webflow ga sam zatvara
   });
 });
 
