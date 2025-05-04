3031250fabbf17cc9393e3741bca6cb595ce3b2e
peca02 <djordjevicpredrag2002@gmail.com>
2025-05-04 03:33:13 +0200

    Update home.js

--- cinema/home.js
+++ cinema/home.js
@@ -93,7 +93,7 @@ async function renderMovies(moviesToShow) {
 
       moviesContainer.style.display = "grid";
       moviesContainer.style.opacity = "0";
-      await sleep(10); // Mali delay da transition registruje promenu
+      await sleep(300); // Mali delay da transition registruje promenu
       moviesContainer.style.opacity = "1";
       await sleep(300);
 
