26bccdcb73c522137898fd033880ab7e376217a6
peca02 <djordjevicpredrag2002@gmail.com>
2025-05-04 19:11:05 +0200

    Update home.js

--- cinema/home.js
+++ cinema/home.js
@@ -95,7 +95,7 @@ async function renderMovies(moviesToShow) {
 
       moviesContainer.style.display = "grid";
         await sleep(300); // kada ovo maknem instant se stvara
-      moviesContainer.style.opacity = "1";
+      // moviesContainer.style.opacity = "1";
       await sleep(300);
 
     } else {
@@ -136,7 +136,7 @@ async function renderMovies(moviesToShow) {
       // Prikaži poruku
       noMoviesMessage.style.display = "flex";
         await sleep(300); // kada ovo maknem instant se stvara
-      noMoviesMessage.style.opacity = "1"; // ovo se instant prikaze ne znam zasto
+      // noMoviesMessage.style.opacity = "1"; // ovo se instant prikaze ne znam zasto
       await sleep(300);
         // kad posle duze vreme promenis filter i treba nestane nesto i da se stvori ubaguje se dom i povuce stranicu nagore, tkd mora uradim samo sa opacity barem za movieskontejner, i da pitam chatgpt koja je razlika
         // izmedju visibility i opacity 0
