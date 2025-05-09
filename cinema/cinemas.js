9e5645b03d782af5db8c8564add717814527a886
peca02 <djordjevicpredrag2002@gmail.com>
2025-05-09 20:19:02 +0200

    Update cinemas.js

--- cinema/cinemas.js
+++ cinema/cinemas.js
@@ -3,3 +3,10 @@ const supabaseUrl = 'https://ypestrqwjqmgkpidrdct.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlwZXN0cnF3anFtZ2twaWRyZGN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1ODQ3MzIsImV4cCI6MjA2MDE2MDczMn0.jIpa-xwIDEdcqBIAFW4NBAtPIvdQRLiaKUwp51p_HkY'
 const supabase = createClient(supabaseUrl, supabaseKey)
 // Sve gore je povezivanje sa Supabase
+
+
+const { data: cinemas, error } = await supabase
+  .from('cinemas')
+  .select('*')
+
+console.log(cinemas);
