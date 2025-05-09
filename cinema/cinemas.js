d61170030025cb5dac6223634215ce029370bc8b
peca02 <djordjevicpredrag2002@gmail.com>
2025-05-09 20:06:04 +0200

    Create cinemas.js

---
+++ cinema/cinemas.js
@@ -0,0 +1,5 @@
+const { createClient } = await import('https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm')
+const supabaseUrl = 'https://ypestrqwjqmgkpidrdct.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlwZXN0cnF3anFtZ2twaWRyZGN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1ODQ3MzIsImV4cCI6MjA2MDE2MDczMn0.jIpa-xwIDEdcqBIAFW4NBAtPIvdQRLiaKUwp51p_HkY'
+const supabase = createClient(supabaseUrl, supabaseKey)
+// Sve gore je povezivanje sa Supabase
