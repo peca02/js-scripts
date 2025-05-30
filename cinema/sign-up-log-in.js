557d0568603b6ecdc7a0faf61bce152f62923df4
peca02 <djordjevicpredrag2002@gmail.com>
2025-05-30 17:42:46 +0200

    Update sign-up-log-in.js

--- cinema/sign-up-log-in.js
+++ cinema/sign-up-log-in.js
@@ -4,8 +4,8 @@ const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS
 const supabase = createClient(supabaseUrl, supabaseKey)
 // Sve gore je povezivanje sa Supabase
 
-//const { data: { user } } = await supabase.auth.getUser();
-//console.log(user);
+const { data: { user } } = await supabase.auth.getUser();
+console.log(user);
 
 const signUpForm = document.getElementById('signup-form');
 const formDone = document.querySelector('.w-form-done');
