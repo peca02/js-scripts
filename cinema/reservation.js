0e88d12442db6700a747f62b03d4a4a5e019a78e
peca02 <djordjevicpredrag2002@gmail.com>
2025-05-31 15:21:09 +0200

    Update reservation.js

--- cinema/reservation.js
+++ cinema/reservation.js
@@ -1,56 +1,32 @@
-function generateCurvedSeats() {
-  const seats = [];
-  const totalRows = 15;
-  const minSeatsInRow = 10;
-  const maxSeatsInRow = 30;
-  const centerX = 500;
-  const centerY = 100; // scena
-  const rowSpacing = 30;
+const { createClient } = await import('https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm')
+const supabaseUrl = 'https://ypestrqwjqmgkpidrdct.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlwZXN0cnF3anFtZ2twaWRyZGN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1ODQ3MzIsImV4cCI6MjA2MDE2MDczMn0.jIpa-xwIDEdcqBIAFW4NBAtPIvdQRLiaKUwp51p_HkY'
+const supabase = createClient(supabaseUrl, supabaseKey)
+// Sve gore je povezivanje sa Supabase
 
-  for (let row = 1; row <= totalRows; row++) {
-    const t = (row - 1) / (totalRows - 1);
-    const seatsInRow = Math.floor(minSeatsInRow + t * (maxSeatsInRow - minSeatsInRow));
-    const radius = rowSpacing * row + 100; // udaljenost od centra (pozornice)
-    const angleStart = -Math.PI / 2 - Math.PI / 3; // -120 stepeni
-    const angleEnd = -Math.PI / 2 + Math.PI / 3;   // +120 stepeni
+const { data, error } = await supabase
+  .from('screenings')
+  .select(`
+    id,
+    start_time,
+    format,
+    language,
+    base_price,
+    movie_id,
+    hall:hall_id (
+      id,
+      name,
+      total_seats,
+      seats:seats (
+        id,
+        row,
+        col,
+        row_label,
+        seat_type_id
+      )
+    )
+  `)
+  .eq('id', screeningId)
+  .single(); // zato što dobijaš samo jednu projekciju
 
-    for (let i = 0; i < seatsInRow; i++) {
-      const angle = angleStart + (i / (seatsInRow - 1)) * (angleEnd - angleStart);
-      const x = centerX + Math.cos(angle) * radius;
-      const y = centerY + Math.sin(angle) * radius;
-
-      seats.push({
-        id: `row${row}_seat${i + 1}`,
-        row: row,
-        number: i + 1,
-        x: Math.round(x),
-        y: Math.round(y),
-        type: 'standard'
-      });
-    }
-  }
-
-  return seats;
-}
-
-const hallSeats = generateCurvedSeats();
-console.log(hallSeats);
-
-const canvas = document.createElement('canvas');
-canvas.id = 'hall';
-canvas.width = 1000;
-canvas.height = 800;
-canvas.style.border = '1px solid gray'; // opcionalno
-
-// 2. Dodaj ga u body
-document.body.appendChild(canvas);
-
-const ctx = canvas.getContext('2d');
-const seats = generateCurvedSeats();
-
-for (const seat of seats) {
-    ctx.beginPath();
-    ctx.arc(seat.x, seat.y, 5, 0, Math.PI * 2);
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-  }
+console.log(data);
