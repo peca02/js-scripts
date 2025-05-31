8632d9ff9caebf38be9c1fd8deee9d596e0d9980
peca02 <djordjevicpredrag2002@gmail.com>
2025-05-31 19:26:31 +0200

    Update reservation.js

--- cinema/reservation.js
+++ cinema/reservation.js
@@ -65,7 +65,6 @@ const sortedRows = Object.entries(rowsMap).sort(([a], [b]) => Number(a) - Number
 
 // 6. Napravi seat map grid div
 const seatMap = document.querySelector('.c-seat-map');
-seatMap.style.display = 'grid';
 seatMap.style.gridTemplateColumns = `repeat(${maxCol + 3}, 1fr)`; // +3 zbog row label + 2 prazna
 
 // 7. Renderuj grid
@@ -76,33 +75,23 @@ for (const [rowNumber, seatsInRow] of sortedRows) {
   // Sortiraj sedista po koloni
   seatsInRow.sort((a, b) => a.col - b.col);
 
-  // Inicijalna kolona za upoređivanje
-  let currentCol = 0;
 
   // ➤ 7.1 Dodaj row label
   const labelDiv = document.createElement('div');
   labelDiv.classList.add('c-row-label');
   labelDiv.textContent = rowLabel;
   seatMap.appendChild(labelDiv);
-  currentCol++;
 
   // ➤ 7.2 Dodaj 2 prazna mesta
   for (let i = 0; i < 2; i++) {
     const empty = document.createElement('div');
     empty.classList.add('c-empty-seat');
     seatMap.appendChild(empty);
-    currentCol++;
   }
 
-  // ➤ 7.3 Dodaj sedišta i praznine ako treba
-  for (const seat of seatsInRow) {
-    while (currentCol < seat.col + 3) {
-      // Prazno mesto
-      const empty = document.createElement('div');
-      empty.classList.add('c-empty-seat');
-      seatMap.appendChild(empty);
-      currentCol++;
-    }
+ // Inicijalna kolona za upoređivanje
+  let currentCol = 0;
+  
 
     const seatDiv = document.createElement('div');
     seatDiv.classList.add('c-seat');
