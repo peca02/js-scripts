c1cb1cbfb48571bc4e97eff39c7ebe4ce5348c8a
Predrag Djordjevic <djordjevicpredrag2002@gmail.com>
2025-06-04 22:30:56 +0200

    Update reservation.js

--- cinema/reservation.js
+++ cinema/reservation.js
@@ -323,5 +323,5 @@ else{
       if (govno2) {
         console.error("RPC error:", govno2);
       } else {
-        console.log("Reservation with seats inserted:", govno);// kaka
+        console.log("Reservation with seats inserted:", govno);// goovnaa
       }
