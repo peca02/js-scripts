d8d44f4c9bbc2a180fba364b50fc1017b6f14140
peca02 <djordjevicpredrag2002@gmail.com>
2025-06-27 01:06:48 +0200

    Update pocetna.js

--- vlada/pocetna.js
+++ vlada/pocetna.js
@@ -3,14 +3,5 @@ const viewer = document.getElementById('heroModel');
 viewer.addEventListener('model-visibility', (e) => {
   if (e.detail.visible) {
     console.log('Model je prisutan i vidljiv na ekranu.');
-
-    // Ovde ubacuješ početak animacije kamere
-    viewer.autoRotate = true;
-
-    setTimeout(() => {
-      viewer.autoRotate = false;
-      viewer.cameraOrbit = '0deg 75deg 2m';
-      viewer.jumpCameraToGoal();
-    }, 1800);
   }
 });
