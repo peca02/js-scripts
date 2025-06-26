da3b41e4ddfaf8920069f35667e6a03045f21d91
peca02 <djordjevicpredrag2002@gmail.com>
2025-06-27 01:32:01 +0200

    Update pocetna.js

--- vlada/pocetna.js
+++ vlada/pocetna.js
@@ -1,16 +1,7 @@
 const viewer = document.getElementById('heroModel');
 
-viewer.addEventListener('model-visibility', async (e) => {
-  if (!e.detail.visible) return;
-
-  console.log('Model je vidljiv, kreće animacija');
-
-  // Kamera će se automatski interpolirati na zadatu orbitu
+viewer.addEventListener('model-visibility', () => {
   viewer.cameraOrbit = '0deg 75deg 2m';
   viewer.autoRotate = false;
-
-  // Opcionalno: nakon 1s (vreme animacije) resetuj rotaciju ako treba
-  setTimeout(() => {
-    viewer.resetTurntableRotation();
-  }, 1100);
+  console.log('Počinje "upad" sa desna na levu!');
 });
