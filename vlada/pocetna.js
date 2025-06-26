fc9ea647312d5c05b885e65646f7672b8366c0a6
peca02 <djordjevicpredrag2002@gmail.com>
2025-06-27 00:50:46 +0200

    Update pocetna.js

--- vlada/pocetna.js
+++ vlada/pocetna.js
@@ -1,16 +1,11 @@
-const modelViewer = document.getElementById('heroModel');
+modelViewer.addEventListener('load', async () => {
+  console.log('Model učitan!');
 
-  modelViewer.addEventListener('load', async () => {
-    // Otkrivanje modela uz smooth prelaz
-    modelViewer.reveal();
+  modelViewer.reveal();
 
-    // Sačekaj malo da model "uđe"
-    await new Promise(r => setTimeout(r, 1800));
+  await new Promise(r => setTimeout(r, 1800));
+  modelViewer.autoRotate = false;
 
-    // Kada uđe, zaustavi rotaciju
-    modelViewer.autoRotate = false;
-
-    // (Po želji) centriraj kameru posle ulaska
-    modelViewer.cameraOrbit = '0deg 75deg 2m';
-    modelViewer.jumpCameraToGoal();
-  });
+  modelViewer.cameraOrbit = '0deg 75deg 2m';
+  modelViewer.jumpCameraToGoal();
+});
