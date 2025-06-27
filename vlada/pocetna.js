0458b59f0c7a427578bad3f956c597659d8fcd0f
peca02 <djordjevicpredrag2002@gmail.com>
2025-06-27 03:01:21 +0200

    Update pocetna.js

--- vlada/pocetna.js
+++ vlada/pocetna.js
@@ -3,7 +3,7 @@ const viewer = document.getElementById('heroModel');
 viewer.addEventListener('model-visibility', () => {
   const start = 180;
   const end = 30;
-  const duration = 10000; // trajanje u milisekundama
+  const duration = 5000; // trajanje u milisekundama
   const startTime = performance.now();
 
   function animate(now) {
