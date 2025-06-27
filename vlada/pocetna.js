502043c940ed48eab40e9d5bfffbacbe7e092f43
peca02 <djordjevicpredrag2002@gmail.com>
2025-06-27 02:34:53 +0200

    Update pocetna.js

--- vlada/pocetna.js
+++ vlada/pocetna.js
@@ -2,7 +2,7 @@ const viewer = document.getElementById('heroModel');
 
 viewer.addEventListener('model-visibility', () => {
 
-  viewer.cameraOrbit = '30deg 50deg 2m'; 
+  viewer.cameraOrbit = '30deg 50deg 6m'; 
   // kamera ide sa desne (ini. 90deg) na centar (0deg)
 
   viewer.autoRotate = false;
