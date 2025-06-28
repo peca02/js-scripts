9ecf26286787a7d782287c4080b69718da600fc4
peca02 <djordjevicpredrag2002@gmail.com>
2025-06-28 15:17:20 +0200

    Update pocetna.js

--- vlada/pocetna.js
+++ vlada/pocetna.js
@@ -2,7 +2,6 @@ const viewer = document.getElementById('heroModel');
 const modelWrapper = document.querySelector('.desna-kolona'); // div koji pomeramo
 
 viewer.addEventListener('load', () => {
-  document.getElementById('govno').innerText = 'govno';
   const start = 180;
   const end = 30;
   const duration = 3000;
