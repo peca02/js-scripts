d6c8df9a7e66539e0916663bd1b5795c64359086
peca02 <djordjevicpredrag2002@gmail.com>
2025-07-04 22:36:20 +0200

    Update pocetna.js

--- vlada/pocetna.js
+++ vlada/pocetna.js
@@ -163,7 +163,7 @@ ScrollSmoother.create({
 	wrapper: "#wrapper",
 	content: "#content",
 	smooth: 1,
-  speed: 3,
+  speed: 2,
 	effects: true,
 	onUpdate: self => skewSetter(clamp(self.getVelocity() / -50)),
 	onStop: () => skewSetter(0)
