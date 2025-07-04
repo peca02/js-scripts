2cd3ee11e4e7271e9ae476ae0ad6a5e432dd22e4
peca02 <djordjevicpredrag2002@gmail.com>
2025-07-05 01:43:47 +0200

    Update pocetna.js

--- vlada/pocetna.js
+++ vlada/pocetna.js
@@ -156,3 +156,36 @@ popupWrapper.addEventListener('click', (event) => {
   google.maps.importLibrary("maps").then(() => initMap());
 })();
 
+const quotes = document.querySelectorAll(".paragraf-pitanje");
+
+function setupSplits() {
+  quotes.forEach((quote) => {
+    // Reset if needed
+    if (quote.anim) {
+      quote.anim.progress(1).kill();
+      quote.split.revert();
+    }
+
+    quote.split = SplitText.create(quote, {
+      type: "words,chars",
+      linesClass: "split-line"
+    });
+
+    // Set up the anim
+    quote.anim = gsap.from(quote.split.chars, {
+      scrollTrigger: {
+        trigger: quote,
+        toggleActions: "restart pause resume reverse",
+        start: "center center",
+        markers: { startColor: "#dfdcff", endColor: "transparent" }
+      },
+      duration: 0.6,
+      ease: "circ.out",
+      y: 80,
+      stagger: 0.02
+    });
+  });
+}
+
+ScrollTrigger.addEventListener("refresh", setupSplits);
+setupSplits();
