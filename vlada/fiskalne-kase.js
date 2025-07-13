2cfa0b47fbaa44d12727d53312b58c5deac2e4fc
peca02 <djordjevicpredrag2002@gmail.com>
2025-07-13 02:55:08 +0200

    Update fiskalne-kase.js

--- vlada/fiskalne-kase.js
+++ vlada/fiskalne-kase.js
@@ -9,7 +9,7 @@ const observer = new IntersectionObserver((entries) => {
       document.fonts.ready.then(() => {
         const split = new SplitType(target, { split: 'words, lines' });
 
-        const words = document.querySelectorAll('.word');
+        const words = target.querySelectorAll('.word');
         words.forEach((word, i) => {
           word.style.setProperty('--i', i);
         });
@@ -38,7 +38,7 @@ const observer2 = new IntersectionObserver((entries) => {
       document.fonts.ready.then(() => {
         const split = new SplitType(target2, { split: 'words, lines' });
 
-        const words = document.querySelectorAll('.word');
+        const words = target2.querySelectorAll('.word');
         words.forEach((word, i) => {
           word.style.setProperty('--i', i);
         });
