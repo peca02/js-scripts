a846d507137c52d5721128e99f1b0149860667a4
peca02 <djordjevicpredrag2002@gmail.com>
2025-07-13 20:41:32 +0200

    Update fiskalne-kase.js

--- vlada/fiskalne-kase.js
+++ vlada/fiskalne-kase.js
@@ -1,1 +1,28 @@
+const target = document.querySelector('.js-split-text');
+let triggered = false;
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting && !triggered) {
+      triggered = true;
+
+      document.fonts.ready.then(() => {
+        const split = new SplitType(target, { split: 'words, lines' });
+
+        const words = document.querySelectorAll('.word');
+        words.forEach((word, i) => {
+          word.style.setProperty('--i', i);
+        });
+
+        target.classList.add('animiraj');
+      });
+
+      observer.unobserve(target); // opciono
+    }
+  });
+}, {
+  root: null,
+  threshold: 1
+});
+
+observer.observe(target);
